d868a5930b5448c595d3f367e1e0f372c59a4e03
DenizYil <denizyil@outlook.dk>
2021-09-28 12:27:52 +0200

    First queries

--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace Assignment3
 {
     public class Queries
     {
+        public static IEnumerable<object> getWizardsByRowlingLINQ() {
+
+            var wizards = Wizard.Wizards.Value;
+
+            var query = from wizard in wizards
+                        where wizard.Creator == "Rowling"
+                        select new {Name = wizard.Name};
+
+            return query;
+        }
+
+        public static IEnumerable<object> getWizardsByRowling() {
+
+            var wizards = Wizard.Wizards.Value;
+
+            var query = wizards.Where(w => w.Creator == "Rowling")
+                               .Select(w => new {Name = w.Name});
+            
+            return query;
+        }
+
+        
+
     }
 }
