b5cefca94b1f34cd350a774f67a21a0907c9d791
DenizYil <denizyil@outlook.dk>
2021-09-28 11:49:56 +0200

    Add test for number equality delegate

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,14 +1,19 @@
 using System;
 using Xunit;
+using Assignment3;
 
 namespace Assignment3.Tests
 {
     public class DelegatesTests
     {
-        /*[Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData(" 0042", 42, true)]
+        [InlineData("54", 54, true)]
+        [InlineData(" 66 ", 66, true)]
+        [InlineData(" 66 ", 65, false)]
+        public void Delegates_Number_Equality_Returns_Correct_Value(string text, int number, bool expected) {
 
-        }*/
+            Assert.Equal(Delegates.NumberEqualityOperation(text, number), expected);
+        }
     }
 }
