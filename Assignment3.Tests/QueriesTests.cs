766ed50fb9756736899145eb417fbf3a3508db2d
Cromski <90246462+Cromski@users.noreply.github.com>
2021-09-30 18:08:11 +0200

    half done test :)

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,25 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment3.Tests
 {
     public class QueriesTests
     {
-        // [Fact]
-        // public void Test1()
-        // {
-        //
-        // }
+        [Fact]
+        public void getWizardsByRowlingLINQ_given_WizardsValue()
+        {
+            //Arrange
+            var wizards = Wizard.Wizards.Value;
+            IEnumerable<object> expected = new List<object>{"Harry Potter", "Harry Potter", "Dumbledore"};
+            IEnumerable<object> actual;
+
+            //Act
+            actual = Queries.getWizardsByRowling();
+
+            //Assert
+            Assert.Equal(expected, actual);
+
+        }
     }
 }
